5549e9efc69f228ec59333fcec6dcb7ab9bb49ba
mdgetahun <henokmgetahun@gmail.com>
2025-07-11 20:04:06 +0200

    Add folder with basic_operations in it

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+subtraction = number1 - number2
+product = number1 * number2
+print("Addition of", number1, "and", number2, "is", result)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", product)
+
