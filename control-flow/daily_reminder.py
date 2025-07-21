9c03dee7c7f03807d15c45477299fc372f6b245c
mdgetahun <henokmgetahun@gmail.com>
2025-07-22 01:50:15 +0200

    Edit daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,8 +2,8 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
 
 while True:
-    time = input("Time bound? (yes/no)").lower()
-    if time == "yes" or time == "y" or time == "no" or time == "n":
+    time_bound = input("Time bound? (yes/no)").lower()
+    if time_bound == "yes" or time_bound == "y" or time_bound == "no" or time_bound == "n":
         break
     else:
         print("Invalid input, please try again")
@@ -13,17 +13,17 @@ while True:
 match priority:
        
         case "high":
-            if time == "yes" or "y":
+            if time_bound == "yes" or "y":
                 print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
             else:
                 print(f"Note: {task} is pending, don't forget!")
         case "medium":
-            if time == "yes" or "y":
+            if time_bound == "yes" or "y":
                 print(f"{task} is a {priority} priority task and must be done soon!")
             else:
                 print(f"Note: {task} is pending, work on it when possible!")
         case "low":
-            if time == "yes" or "y":
+            if time_bound == "yes" or "y":
                 print(f"Gentle Reminder: {task} is a {priority} priority task. Keep in mind!")
             else:
-                print(f"Note! {task} is a {priority} task. Consider completing it when you have free time.")
+                print(f"Note! {task} is a {priority} task. Consider completing it when you have free time_bound.")
